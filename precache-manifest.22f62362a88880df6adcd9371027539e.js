55203037139cac7039aabb34233b7ff54dead659
huudangdev <nguyentranhuudang.blog@gmail.com>
2019-07-24 10:44:28 +0700

    Updates

---
+++ precache-manifest.22f62362a88880df6adcd9371027539e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472f4787404cf05a92e92b142074b707",
+    "url": "/clients-chat/index.html"
+  },
+  {
+    "revision": "c8a487debb778cc2ce67",
+    "url": "/clients-chat/static/css/main.2d9da3cd.chunk.css"
+  },
+  {
+    "revision": "6ee1392689f075764ccf",
+    "url": "/clients-chat/static/js/2.e627a644.chunk.js"
+  },
+  {
+    "revision": "c8a487debb778cc2ce67",
+    "url": "/clients-chat/static/js/main.3ef8ba5b.chunk.js"
+  },
+  {
+    "revision": "6f27e227d2607b4663e0",
+    "url": "/clients-chat/static/js/runtime~main.5e5c54a1.js"
+  }
+]);
